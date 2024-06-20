3829d01325092dc613239da51b27dc04a1066bc8
Ben Daali <bendaalimehdi@gmail.com>
2024-06-20 17:04:41 +0200

    20-06

--- force-app/main/default/lwc/massFileDownloader/massFileDownloader.js
+++ force-app/main/default/lwc/massFileDownloader/massFileDownloader.js
@@ -1,3 +1,5 @@
+import { refreshApex } from '@salesforce/apex';
+import deleteFiles from "@salesforce/apex/MassFileDownloaderController.deleteFiles";
 import getFiles from "@salesforce/apex/MassFileDownloaderController.getFiles";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { LightningElement, api, track, wire } from "lwc";
@@ -29,14 +31,14 @@ export default class MassFileDownloader extends LightningElement {
   selectedFileIds = [];
 
   @wire(getFiles, { opportunityId: "$recordId" })
-  wiredFiles({ data, error }) {
-    if (data) {
-      this.files = data;
-      this.error = undefined;
-    } else if (error) {
-      this.error = error;
-      this.files = [];
-    }
+  wiredFiles;
+
+  get wiredFilesData() {
+    return this.wiredFiles.data;
+  }
+
+  get wiredFilesError() {
+    return this.wiredFiles.error;
   }
 
   downloadFiles() {
@@ -50,6 +52,24 @@ export default class MassFileDownloader extends LightningElement {
     this.initDownloading(this.getDownloadString(selectedFiles));
   }
 
+  deleteFiles() {
+    let selectedFiles = this.getSelectedRows();
+
+    if (selectedFiles.length === 0) {
+      this.showToast("Error", "No files selected", "error");
+      return;
+    }
+
+    deleteFiles({ fileIds: selectedFiles.map((file) => file.Id) })
+      .then(() => {
+        this.showToast("Success", "Files deleted successfully", "success");
+        return refreshApex(this.wiredFiles);
+      })
+      .catch((error) => {
+        this.showToast("Error", error.body.message, "error");
+      });
+  }
+
   getDownloadString(files) {
     let downloadString = "";
     files.forEach((item) => {
@@ -60,7 +80,7 @@ export default class MassFileDownloader extends LightningElement {
 
   initDownloading(downloadString) {
     window.open(BASE_DOWNLOAD_PATH + downloadString, "_blank");
-    this.showToast("DONE", downloadString);
+    this.showToast("DONE", downloadString, "success");
   }
 
   getSelectedRows() {
